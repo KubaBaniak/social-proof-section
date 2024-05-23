9ca2ec6a4c86ee6f811991b1b6eea901b0848ae0
Jakub Urbaniak <kuba121201@gmail.com>
2024-05-23 08:06:20 +0200

    refactor: project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import "./App.scss";
-import Rating from "./components/rating/rating";
-import Recommendation from "./components/recommendation/recommendation";
+import LandingPage from "./pages/landing-page";
 
-const lorem =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac elementum quam, sit amet rhoncus erat. Etiam nec efficitur elit, id rutrum libero. Donec sed condimentum ipsum.";
-
-function App() {
+export default function App() {
   return (
-    <div className="main">
-      <div className="background-top"></div>
-      <div className="main-content">
-        <div className="text-section">
-          <h1>10,000+ of our uses love our products.</h1>
-          <span>
-            We only provide great products combined with excellent customer
-            service. See what our satisfied customers are saying about our
-            services
-          </span>
-        </div>
-        <div className="ratings">
-          <Rating service="Reviews" />
-          <Rating service="Report Guru" />
-          <Rating service="BestTech" />
-        </div>
-      </div>
-      <div className="recommendation-section">
-        <Recommendation name="Colton Smith" text={lorem} />
-        <Recommendation name="Irene Roberts" text={lorem} />
-        <Recommendation name="Anne Wallace" text={lorem} />
-      </div>
-      <div className="background-bottom"></div>
-    </div>
+    <>
+      <LandingPage />
+    </>
   );
 }
-
-export default App;
